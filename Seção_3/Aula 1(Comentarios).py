2b779d94221c1a0499591a7c3cfcd87b54eecb8f
Eduardo Abrantes <eduardopabrantes@gmail.com>
2023-11-06 11:01:47 -0300

    commit do prieiro dia

---
+++ Seção_3/Aula 1(Comentarios).py
@@ -0,0 +1,12 @@
+# Comentario do python
+
+# Print do python: 
+print(123); print(456);
+
+"""
+
+eai galera
+vamo nessa
+
+
+"""
---
+++ Seção_3/Aula_2(Print).py
@@ -0,0 +1,15 @@
+"""
+Print é o meio de imprimir alguma mensagem no terminal
+Ela como as outras ferramentas de programação como repetidores e
+estruturas de decisão são funções/metodos então eles precisao de argumentos
+para funcionarem.
+"""
+#argumentos não nomeados (18 e 69)
+#    |
+#    \/
+print("Edu Goiaba","Incrivel", sep="-", end="!!\n")
+print(56, 78, sep='-', end="!!\n")
+
+
+
+
