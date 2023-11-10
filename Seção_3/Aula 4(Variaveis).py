5c23f8ac2b84b9e69ca9240ef44c0cc1a6cc5ee9
Eduardo Abrantes <eduardopabrantes@gmail.com>
2023-11-10 20:29:22 -0300

    Variaveis

---
+++ Seção_3/Aula 4(Variaveis).py
@@ -0,0 +1,43 @@
+#variavel é um lugar na memoria que armazena um valor
+#de algum tipo como int, float, string e etc.
+
+
+#PRÁTICA:
+
+#nome_completo = 'Eduardo Pereira Abrantes'
+#soma_dois_mais_dois = 2 + 2
+#int_um = '1'
+#print(int(int_um), type(int(int_um)))
+#print(nome_completo, soma_dois_mais_dois)
+
+
+#PRATICA 2:
+
+#nome = 'Edu'
+#idade = 16
+#maior_de_idade = idade >= 18
+#print('Nome: ', nome, 'Idade: ', idade)
+#print('É maior?', maior_de_idade)
+
+
+#EXERCICIO:
+
+nome = 'Edu'
+sobrenome = 'Goiaba'
+idade = 18
+ano_nascimento = 2005
+maior_de_idade = idade >= 18
+altura_metros = 1,80
+
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('AnoNascimento:', ano_nascimento)
+print('É maior de idade?','\n',maior_de_idade, sep='' )
+print('Altura em metros:', altura_metros, 'metros')
+
+
+
+
+
