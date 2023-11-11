d826f5212007c2d4cea4e6453b6cf679b35bf313
Eduardo Abrantes <eduardopabrantes@gmail.com>
2023-11-11 08:44:06 -0300

    Input, operadores_de_decição e debbug_python

---
+++ Seção_3/Aula_7(Input_slash_scanf_slash_console.log.writeKey).py
@@ -0,0 +1,16 @@
+
+#O comando printe faz com que dados sejam impressos no terminal
+#O comando input fara com que o usuario possa inserir algum dado do terminal para o codigo
+
+nome = input("Qual o seu nome? ")
+print(f'Seu nome é {nome = }')
+
+
+numero_1 = input("Digite um numero: ")
+numero_2 = input("Digite um numero: ")
+
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'A soma dos numeros é: {int_numero_1 + int_numero_2}')
---
+++ Seção_3/Aula_8(if_slash_elif e else).py
@@ -0,0 +1,11 @@
+
+entrada = input("Você quer 'entrar' ou 'sair'? ")
+
+if entrada == 'entrar':
+    print('Voce entrou no sistema')
+elif entrada == 'sair':
+    print('Voce saiu do sistema')
+else :
+    print('Digite "entrar" ou "sair"')
+
+print("FORA DOS BLOCOS")
